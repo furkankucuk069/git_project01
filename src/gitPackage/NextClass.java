cb41b14b34d5b2dbc3ef58a3608feee24102409e
furkankucuk069 <62223333+furkankucuk069@users.noreply.github.com>
2020-03-16 19:53:45 -0400

    commit 9

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -2,7 +2,7 @@ package gitPackage;
 
 public class NextClass {
 	public static void main(String [] args ) {
-		System.out.println("Hello dear friends"); 	
+		System.out.println("Hello dear SDETS"); 	
 	
 	
 	
